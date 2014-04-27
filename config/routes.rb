53581f43856ececdac8fde141df9714523a9ef8e
Oliver Bridgman <oli.bridgman@gmail.com>
2014-04-27 16:32:20 +1200

    root

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Tally::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
+  root 'static_pages/home'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
