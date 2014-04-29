f97c81c659a65846f252176379a2421244bed624
Oliver Bridgman <oli.bridgman@gmail.com>
2014-04-29 19:19:30 +1200

    user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ActiveRecord::Base
 	before_save { self.email = email.downcase }
+	has_secure_password
 	validates :name, presence: true, length: { maximum: 50 }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :email, presence: true, 
 					  format: { with: VALID_EMAIL_REGEX }, 
 					  uniqueness: { case_sensitive: false }
+	validates :password, length: { minimum: 6 }
 
 end
